b703f78e2ad9096c9523a4d2137c7777032b75c0
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-03 17:22:52 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Bowling Scoring!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
