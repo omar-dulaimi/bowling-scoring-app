27147c2a8ab20b2947f684187f541a6a3e09b8d6
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-05 15:25:53 +0300

    add helpers

---
+++ src/components/Game/helpers/index.ts
@@ -0,0 +1,132 @@
+import { FrameType, GameDataType, ThrowType } from "../../../types";
+
+export const calculateFrameScore = (
+  frame: FrameType,
+  index: number,
+  gameData: GameDataType
+) => {
+  const nextFrame = gameData.frames?.[index + 1];
+  const thenFrame = gameData.frames?.[index + 2];
+  let result = 0;
+  const isLastFrame = index === 9;
+
+  if (isLastFrame) {
+    if (frame.isStrike && frame.isSpare) {
+      result += 20;
+    } else if (frame.isSpare && !frame.isStrike) {
+      result += 10 + Number(frame.thirdThrowSCore);
+    } else if (frame.isStrike && !frame.isSpare) {
+      result +=
+        Number(frame.firstThrowScore) +
+        Number(frame.secondThrowScore) +
+        Number(frame.thirdThrowSCore);
+    }
+  } else {
+    if (frame.isStrike) {
+      result += 10;
+      if (nextFrame?.isStrike) {
+        if (nextFrame?.canHaveThirdThrow) {
+          if (nextFrame?.firstThrowScore === 10) {
+            result += 10;
+            if (
+              Number(nextFrame?.secondThrowScore) +
+                Number(nextFrame?.thirdThrowSCore) ===
+              10
+            ) {
+              result += 10;
+            } else {
+              result += Number(nextFrame?.secondThrowScore);
+            }
+          } else {
+            if (
+              Number(nextFrame?.firstThrowScore) +
+                Number(nextFrame?.secondThrowScore) ===
+              10
+            ) {
+              result += 10;
+              result += Number(nextFrame?.thirdThrowSCore);
+            } else {
+              result +=
+                Number(nextFrame?.firstThrowScore) +
+                Number(nextFrame?.secondThrowScore);
+            }
+          }
+        } else {
+          result += 10;
+          if (thenFrame?.isStrike) {
+            result += 10;
+          } else {
+            result += Number(thenFrame?.firstThrowScore);
+          }
+        }
+      } else {
+        result +=
+          Number(nextFrame?.firstThrowScore) +
+          Number(nextFrame?.secondThrowScore);
+      }
+    } else if (frame.isSpare) {
+      result += 10;
+      if (nextFrame?.isStrike) {
+        result += 10;
+      } else {
+        result += Number(nextFrame?.firstThrowScore);
+      }
+    } else {
+      result += Number(frame.firstThrowScore) + Number(frame.secondThrowScore);
+    }
+  }
+  return result;
+};
+
+export const getCardPointerEventsValue = (
+  frame: FrameType,
+  index: number,
+  currentFrameIndex: number
+) => {
+  if (frame.canHaveThirdThrow) {
+    if (!frame.isStrike && !frame.isSpare) {
+      if (frame.firstThrowScore !== null && frame.secondThrowScore !== null) {
+        return "none";
+      }
+    } else if (frame.isStrike || frame.isSpare) {
+      if (
+        frame.firstThrowScore !== null &&
+        frame.secondThrowScore !== null &&
+        frame.thirdThrowSCore !== null
+      ) {
+        return "none";
+      }
+    }
+  } else {
+    return frame.isStrike || index !== currentFrameIndex ? "none" : "auto";
+  }
+};
+
+export const getNumbersRange = (frame: FrameType, forThrow: ThrowType) => {
+  if (forThrow === "1st") {
+    return [0, 10];
+  }
+
+  if (forThrow === "2nd") {
+    if (frame.canHaveThirdThrow) {
+      if (frame.firstThrowScore === 10) {
+        return [0, 10];
+      } else {
+        return [0, 10 - Number(frame.firstThrowScore)];
+      }
+    } else {
+      return [0, 10 - Number(frame.firstThrowScore)];
+    }
+  }
+
+  if (forThrow === "3rd") {
+    if (frame.canHaveThirdThrow) {
+      if (frame.secondThrowScore === 10) {
+        return [0, 10];
+      } else {
+        return [0, 10 - Number(frame.secondThrowScore)];
+      }
+    }
+  }
+  return [0, 10];
+};
