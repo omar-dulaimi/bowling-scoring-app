7fab1490b55bb3fcd60348eed756447a88b14089
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-05 15:25:25 +0300

    add types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type FrameType = {
+  name: string;
+  frameScore: number | null;
+  firstThrowScore: number | null;
+  secondThrowScore: number | null;
+  thirdThrowSCore: number | null;
+  isSpare: boolean;
+  isStrike: boolean;
+  isDoneScoring: boolean;
+  canHaveThirdThrow: boolean;
+};
+
+export type GameDataType = {
+  frames: FrameType[];
+};
+
+export type ThrowType = "1st" | "2nd" | "3rd";
